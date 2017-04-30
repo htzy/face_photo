bdfd503c2ad46abeed86fda5ff9bb119f265d825
htzy <ShiheHuang@foxmail.com>
2017-04-30 16:09:44 +0800

    扫描图片文件夹下的所有jpg照片，并将结果裁剪到results文件夹下

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
 # coding:utf-8
 from __future__ import print_function
 
+import os
+from glob import glob
+
 import cv2
 
-# 待检测的图片路径
-# imagepath = 'images/download.jpg'
-imagepath = 'images/basketball.jpg'
-# TODO 加入爬虫，自动爬取图片
 
-# 获取训练好的人脸的参数数据，这里直接从GitHub上使用默认值
-face_cascade = cv2.CascadeClassifier(r'./haarcascade_frontalface_default.xml')
+def detect(filename):
+    # 获取训练好的人脸的参数数据，这里直接从GitHub上使用默认值
+    face_cascade = cv2.CascadeClassifier(r'./haarcascade_frontalface_default.xml')
 
-# 读取图片
-image = cv2.imread(imagepath)
+    # 读取图片
+    image = cv2.imread(filename)
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# 探测图片中的人脸
-faces = face_cascade.detectMultiScale(
-    gray,
-    scaleFactor=1.15,
-    minNeighbors=5,
-    minSize=(5, 5),
-    flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-)
+    # 探测图片中的人脸
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.15,
+        minNeighbors=5,
+        minSize=(5, 5),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
 
-print("发现{0}个人脸!".format(len(faces)))
+    print("发现{0}个人脸!".format(len(faces)))
 
-for i, (x, y, w, h) in enumerate(faces):
-    face = image[y: y + h, x: x + w, :]
-    face = cv2.resize(face, (96, 96))
-    cv2.imwrite("tem_" + str(i) + ".jpg", face)
-    i += 1
+    for i, (x, y, w, h) in enumerate(faces):
+        face = image[y: y + h, x: x + w, :]
+        face = cv2.resize(face, (96, 96))
+        cv2.imwrite("results/" + os.path.basename(filename).split('.')[0] + "_" + str(i) + ".jpg", face)
+        i += 1
+
+
+if __name__ == '__main__':
+    if os.path.exists('results') is False:
+        os.mkdir('results')
+
+    file_list = glob('images/liguangfu/*.jpg')
+    for filename in file_list:
+        detect(filename)
